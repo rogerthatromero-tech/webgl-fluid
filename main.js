2e377a8df84e709d79b8575857eda9b7ff26e6df
rogerthatromero-tech <rogerthatromero@gmail.com>
2025-12-03 21:41:44 -0500

    Update main.js

--- main.js
+++ main.js
@@ -50,7 +50,7 @@
     var radius = 4.0;
     var azimuth = 0.5*Math.PI;
     var elevation = -0.08134; // midpoint between water surface (0) and pool bottom (~-0.65)
-    var fov = 25.0;
+    var fov = 20.0;
     var eye = sphericalToCartesian(radius, azimuth, elevation);
     var center = [0.0, 0.0, 0.0];
     var up = [0.0, 1.0, 0.0];

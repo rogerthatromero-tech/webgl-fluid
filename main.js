cb9ba3cd1c2a66979f2d1e8e1fc81f76cedcf244
rogerthatromero-tech <rogerthatromero@gmail.com>
2025-12-04 03:10:34 -0500

    Update main.js

--- main.js
+++ main.js
@@ -89,7 +89,7 @@
     var spearCount = 5;
 // X-offsets (left → right from the camera point of view). Tweak these to
 // line up with the 5 tips of your 3D model.
-var spearOffsets = [-0.45, -0.20, 0.0, 0.20, 0.45];
+var spearOffsets = [-0.50, -0.20, 0.0, 0.20, 0.50];
 
 var spearCenters = [];
 var spearOldCenters = [];
